a2a4ef85b0f6f36313cab0fd1dc0815e6a9def95
Anwar <anwarhussaini160@gmail.com>
2023-06-05 15:57:54 +0430

    Refactor JavaScript file

---
+++ js/main.js
@@ -0,0 +1,121 @@
+class Store {
+  static init() {
+    const store = new Store();
+    return store;
+  }
+
+  constructor() {
+    document.querySelector('form').addEventListener('submit', (e) => {
+      e.preventDefault();
+      this.addBook();
+    });
+
+    this.bookList = [];
+    this.stroageName = 'library';
+    this.loadBook();
+    this.diplayBooks();
+    this.configNavItems();
+  }
+
+  addBook() {
+    const bookTitle = document.querySelector('#book-title');
+    const bookAuthor = document.querySelector('#book-author');
+
+    const book = {
+      bookTitle: bookTitle.value,
+      bookAuthor: bookAuthor.value,
+    };
+
+    this.bookList.push(book);
+    this.saveBook();
+    this.loadBook();
+
+    bookTitle.value = '';
+    bookAuthor.value = '';
+  }
+
+  diplayBooks() {
+    const bookContainer = document.querySelector('.container--book');
+    bookContainer.innerHTML = '';
+    this.bookList.forEach((eachBook, index) => {
+      const book = document.createElement('li');
+      book.classList = 'row border border-success-subtle rounded-2 align-items-center p-2 mt-2';
+      if (index % 2 === 1) {
+        book.classList.add('bg-light');
+      } else {
+        book.classList.add('bg-success-subtle');
+      }
+
+      const bookDiv = document.createElement('div');
+      bookDiv.classList = 'col col-12 col-md-8 mb-3 mb-md-0 text-center text-md-start';
+
+      const bookParagraph = document.createElement('p');
+      bookParagraph.classList = 'book-title mb-0';
+      bookParagraph.innerHTML = `<strong>${eachBook.bookTitle}</strong> by <strong>${eachBook.bookAuthor}</strong>`;
+      bookDiv.append(bookParagraph);
+
+      const buttonDiv = document.createElement('div');
+      buttonDiv.classList = 'col col-12 col-md-4';
+
+      const removeButton = document.createElement('button');
+      removeButton.textContent = 'Remove';
+      removeButton.classList = 'btn btn-danger w-100';
+      buttonDiv.appendChild(removeButton);
+
+      book.appendChild(bookDiv);
+      book.appendChild(buttonDiv);
+
+      removeButton.addEventListener('click', () => {
+        this.removeBook(index);
+      });
+
+      bookContainer.appendChild(book);
+    });
+  }
+
+  removeBook(index) {
+    this.bookList.splice(index, 1);
+    this.saveBook();
+    this.loadBook();
+  }
+
+  loadBook() {
+    const localStorageBooks = localStorage.getItem(this.stroageName);
+    if (localStorageBooks) {
+      this.bookList = JSON.parse(localStorageBooks);
+      this.diplayBooks();
+    }
+  }
+
+  saveBook() {
+    localStorage.setItem(this.stroageName, JSON.stringify(this.bookList));
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  configNavItems() {
+    const sections = document.querySelectorAll('section');
+    const navItems = document.querySelectorAll('.btn--nav');
+
+    navItems.forEach((curBtn, index) => {
+      curBtn.addEventListener('click', () => {
+        for (let i = 0; i < sections.length; i += 1) {
+          if (i === index) {
+            sections[i].classList.remove('d-none');
+            navItems[i].classList.add('btn-success');
+            navItems[i].classList.remove('btn-light');
+          } else {
+            sections[i].classList.add('d-none');
+            navItems[i].classList.remove('btn-success');
+            navItems[i].classList.add('btn-light');
+          }
+        }
+      });
+    });
+  }
+}
+
+Store.init();
+
+const nowEl = document.getElementById('now');
+const nowDate = new Date();
+nowEl.textContent = nowDate;
