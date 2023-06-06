5786a7ca940b418cbd89b22c4919b8f5382a2798
Anwar <anwarhussaini160@gmail.com>
2023-06-06 07:46:01 +0430

    Fix the date issue

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import Library from './modules/library.js';
 import navigateMenuItems from './modules/menu_navigation.js';
-import setCurrentDateAndTime from './modules/luxon-date-and-time.js';
+// import setCurrentDateAndTime from './modules/luxon-date-and-time.js';
 
 navigateMenuItems();
 Library.init();
-setCurrentDateAndTime();
+
+// setCurrentDateAndTime();
+const now = document.getElementById('now');
+now.textContent = new Date();
